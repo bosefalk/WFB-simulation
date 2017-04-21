e97f959d8171d94d926979eec271ae654519877a
Unknown <bosefalk@gmail.com>
2017-04-21 16:15:09 +0200

    Removed absolute path to results.csv

--- read_csv.py
+++ read_csv.py
@@ -3,7 +3,7 @@ from pandas import read_csv
 def win_percent():
 
     # Read the csv produced by wfb_simulation.py
-    df = read_csv('~/Projects/WFB-simulation/results.csv')
+    df = read_csv('results.csv')
 
     # Tbl records the number of wins, total is number of simulated battles
     tbl = df['Winner'].value_counts()
